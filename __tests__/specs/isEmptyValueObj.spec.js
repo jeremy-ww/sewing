4e8656cf9eb20cf98c4008e9fa34997f0e859c38
伍程凯 <wuchengkai0@gmail.com>
2018-08-09 15:53:07 +0800

    feat(isEmptyValueObj): support `depth` option

--- __tests__/specs/isEmptyValueObj.spec.js
+++ __tests__/specs/isEmptyValueObj.spec.js
@@ -3,10 +3,39 @@ import isEmptyValueObj from 'sewing/isEmptyValueObj'
 describe('isEmptyValueObj method', function () {
 
   it('should find the empty object', function () {
-    expect(isEmptyValueObj({ name: '', age: 0 })).toBe(true)
-    expect(isEmptyValueObj(null)).toBe(true)
-    expect(isEmptyValueObj({})).toBe(true)
+    expect(isEmptyValueObj(0)).toBe(false)
+    expect(isEmptyValueObj(false)).toBe(false)
+    expect(isEmptyValueObj('string')).toBe(false)
 
-    expect(isEmptyValueObj({ name: 'tom', age: 0 })).toBe(false)
+    expect(isEmptyValueObj('')).toBe(true)
+    expect(isEmptyValueObj(null)).toBe(true)
+    expect(isEmptyValueObj(undefined)).toBe(true)
+    expect(isEmptyValueObj([1, 2, 3])).toBe(false)
+    expect(isEmptyValueObj([1, 2, undefined])).toBe(false)
+    expect(isEmptyValueObj([1, 2, null])).toBe(false)
+    expect(isEmptyValueObj({})).toBe(true)
+    expect(isEmptyValueObj({ name: 'tom' })).toBe(false)
+
+    expect(
+      isEmptyValueObj({
+        name: 'lee',
+        age: 18,
+        empty_object: [{ name: undefined }]
+      })
+    ).toBe(false)
+    expect(
+      isEmptyValueObj({
+        name: 'lee',
+        age: 18,
+        empty_object: [{ name: undefined }]
+      }, 1)
+    ).toBe(true)
+    expect(
+      isEmptyValueObj({
+        name: 'lee',
+        age: 18,
+        empty_object: [{ person: { name: 'lee' } }]
+      }, 1)
+    ).toBe(false)
   })
 })
--- src/isEmptyValueObj.ts
+++ src/isEmptyValueObj.ts
@@ -1,26 +1,44 @@
+import isPrimitive from './isPrimitive'
 import isEmpty from './isEmpty'
 
 /**
- * 判断对象是否为空, 或者对象的一级属性上是否有为空的 value
- * @param  {Object}  obj 判断的对象
- * @return {boolean}     当前对象是否存在空值
+ * 判断对象是否为空, 或者判断对象不同层级上是否有为空的值
+ * @param  {Object}  obj   判断的对象
+ * @param  {number}  depth 递归判断对象的层级
+ * @return {boolean}       当前对象是否存在空值
  *
  * @example
- *
- * isEmptyValueObj(null)
- * // => true
- *
- * isEmptyValueObj({})
- * // => true
  *
  * isEmptyValueObj({ name: 'tom', age: 0 })
  * // => false
  *
  * isEmptyValueObj({ name: '', age: 0 })
  * // => true
+ *
+ * isEmptyValueObj({
+ *   name: 'lee',
+ *   age: 18,
+ *   empty_object: [{ name: undefined }]
+ * })
+ * // => false
+ *
+ * isEmptyValueObj({
+ *   name: 'lee',
+ *   age: 18,
+ *   empty_object: [{ name: undefined }]
+ * }, 1)
+ * // => true
  */
-export default function isEmptyValueObj (obj: any): boolean {
-  if (isEmpty(obj) || !Object.keys(obj).length) return true
+export default function isEmptyValueObj (obj: any, depth = 0): boolean {
+  if (isEmpty(obj)) return true
 
-  return !Object.values(obj).every(v => !isEmpty(v))
+  const keys = Object.keys(obj)
+  let recursionIndex = 0
+
+  return keys.length > 0 && keys.some(key => {
+    const value = obj[key]
+    return isPrimitive(value) || recursionIndex >= depth
+      ? isEmpty(value)
+      : (++recursionIndex && isEmptyValueObj(value)) as boolean
+  })
 }
